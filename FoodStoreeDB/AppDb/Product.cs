62a25741159f118b21e475a52efc929684a44e1d
cacoinchik <mr.ruslan126@mail.ru>
2023-02-02 15:04:33 +0300

    Добавьте файлы проекта.

---
+++ FoodStoreeDB/AppDb/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStoreeDB.AppDb
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+        public string Provider { get; set; }
+        public string ProductType { get; set; }
+    }
+}
---
+++ FoodStoreeDB/AppDb/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStoreeDB.AppDb
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }   
+    }
+}
